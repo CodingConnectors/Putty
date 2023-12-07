6a2ad9fc72f41007c6416d86a86b36d8733c0250
Administrator <Administrator@304-10>
2023-12-07 17:47:50 +0900

    12071747

--- src/main/resources/static/js/views/basket.js
+++ src/main/resources/static/js/views/basket.js
@@ -5,7 +5,22 @@ let checkboxes = document.querySelectorAll('input[name="selectOrder"]');
 checkboxes.forEach(function(checkbox) {
   checkbox.addEventListener('change', calculateTotal);
 });
-
+/*volumeBox.forEach(function(volume) {
+  volume.addEventListener('change', updateProductPerTotal(volume));
+});*/
+let volumeBox = document.querySelectorAll('input[name="product_volume"]');
+volumeBox.forEach(function (input) {
+    input.addEventListener('change', function() {
+		updateProductPerTotal(input);
+		calculateTotal;
+		});
+});
+function updateProductPerTotal(volume) {
+	let price = volume.closest('tr').querySelector('.selectOrder').getAttribute("data-price");
+	let perTotal = volume.value * price;
+	volume.closest('tr').querySelector('.productPerTotal')
+		.textContent=perTotal.toLocaleString('ko-KR') +"원"
+}
 function calculateTotal() {
   // 체크된 체크박스만 가져옴
   let checkedCheckboxes = document.querySelectorAll('input[name="selectOrder"]:checked');
@@ -17,12 +32,15 @@ function calculateTotal() {
   checkedCheckboxes.forEach(function(checkbox) {
     // 각 체크된 상품의 가격을 가져와서 더합니다.
     let price = parseInt(checkbox.getAttribute("data-price"));
-    totalPrice += price;
+    let volume = checkbox.closest('tr').querySelector('.product_volume').value;;
+    perTotal = price*volume;
+    
+    totalPrice += (perTotal);
   });
 
   // 결과를 화면에 출력합니다.
   let totalOutput = document.getElementById("totalPrice");
-  totalOutput.textContent = totalPrice+"원";
+  totalOutput.textContent = totalPrice.toLocaleString('ko-KR') +"원";
   
 }
 
