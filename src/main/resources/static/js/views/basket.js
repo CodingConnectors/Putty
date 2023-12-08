f499b207084f9a19ddd229289924545250823649
Administrator <Administrator@304-10>
2023-12-08 15:25:08 +0900

    12081524kbw

--- src/main/resources/static/js/views/basket.js
+++ src/main/resources/static/js/views/basket.js
@@ -5,22 +5,34 @@ let checkboxes = document.querySelectorAll('input[name="selectOrder"]');
 checkboxes.forEach(function(checkbox) {
   checkbox.addEventListener('change', calculateTotal);
 });
-/*volumeBox.forEach(function(volume) {
-  volume.addEventListener('change', updateProductPerTotal(volume));
-});*/
-let volumeBox = document.querySelectorAll('input[name="product_volume"]');
-volumeBox.forEach(function (input) {
-    input.addEventListener('change', function() {
-		updateProductPerTotal(input);
-		calculateTotal;
-		});
+document.querySelectorAll('.product_volume').forEach(function (input) {
+    input.addEventListener('change', function () {
+        updateProductPerTotal(input);
+        calculateTotal();
+    });
+});
+
+function updateProductPerTotal(volume) {
+    let price = volume.closest('tr').querySelector('.selectOrder').getAttribute("data-price");
+    let discount = volume.value * price / volume.closest('tr').querySelector('.selectOrder').getAttribute("data-discount");
+    let perTotal =  volume.value * price - discount;
+    volume.closest('tr').querySelector('.productPerTotal')
+        .textContent = perTotal.toLocaleString('ko-KR') + "원";
+    volume.closest('tr').querySelector('.discount')
+        .textContent = discount.toLocaleString('ko-KR');
+    volume.closest('tr').querySelector('.discountPer')
+        .textContent = volume.closest('tr').querySelector('.selectOrder').getAttribute("data-discount")+"%";
+}
+/*$(".product_volume").on("change", function() {
+    updateProductPerTotal($(this));
 });
 function updateProductPerTotal(volume) {
-	let price = volume.closest('tr').querySelector('.selectOrder').getAttribute("data-price");
-	let perTotal = volume.value * price;
-	volume.closest('tr').querySelector('.productPerTotal')
-		.textContent=perTotal.toLocaleString('ko-KR') +"원"
-}
+    let price = volume.closest('tr').find('.selectOrder').data("price");
+    let perTotal = volume.val() * price; // Use volume.val() to get the input value
+    volume.closest('tr').find('.productPerTotal')
+        .text(perTotal.toLocaleString('ko-KR') + "원");
+}// 제이쿼리 */ 
+
 function calculateTotal() {
   // 체크된 체크박스만 가져옴
   let checkedCheckboxes = document.querySelectorAll('input[name="selectOrder"]:checked');
@@ -28,19 +40,28 @@ function calculateTotal() {
   updateSelectedItemCount(checkedCheckboxes);
   
   let totalPrice = 0;
+  let totalDisCount = 0;
 
   checkedCheckboxes.forEach(function(checkbox) {
     // 각 체크된 상품의 가격을 가져와서 더합니다.
     let price = parseInt(checkbox.getAttribute("data-price"));
     let volume = checkbox.closest('tr').querySelector('.product_volume').value;;
-    perTotal = price*volume;
     
+    let perTotal = price*volume;
+    let disCount = perTotal / 10;
+    
+    
+    totalDisCount += disCount;
     totalPrice += (perTotal);
   });
 
   // 결과를 화면에 출력합니다.
   let totalOutput = document.getElementById("totalPrice");
   totalOutput.textContent = totalPrice.toLocaleString('ko-KR') +"원";
+  let totalOutputDC = document.getElementById("totalDiscount");
+  totalOutputDC.textContent = "-" + totalDisCount.toLocaleString('ko-KR') +"원";
+  let totalOrderPrice = document.getElementById("totalOrderPrice");
+  totalOrderPrice.textContent = (totalPrice-totalDisCount).toLocaleString('ko-KR') +"원";
   
 }
 
